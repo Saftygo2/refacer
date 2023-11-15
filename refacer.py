0baa9d0951bdeb608ee22efd82fbd1164a779854
Saftygo2 <123340687+Saftygo2@users.noreply.github.com>
2023-11-16 00:50:46 +0100

    Update refacer.py

--- refacer.py
+++ refacer.py
@@ -195,7 +195,7 @@ class Refacer:
         frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fourcc = cv2.VideoWriter_fourcc(*'h264')
         output = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
         
         frames=[]
